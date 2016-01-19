1127f169108cbbef4793b13f7be1ece6892c5d38
Maxime BOMPOIL <mbompoil@e1r2p15.42.fr>
2016-01-19 16:35:59 +0100

    readv2

--- ft_read.c
+++ ft_read.c
@@ -6,13 +6,35 @@
 /*   By: mbompoil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 16:06:30 by mbompoil          #+#    #+#             */
-/*   Updated: 2016/01/19 16:19:38 by mbompoil         ###   ########.fr       */
+/*   Updated: 2016/01/19 16:33:30 by mbompoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+int		ft_count(int argc, char **argv)
+{
+	int		x;
+	int		ret;
+	int		fd;
+	char	buf;
+	int		i;
+
+	i = 0;
+	x = 1;
+	while (x < argc)
+	{
+		fd = open(argv[x], O_RDONLY);
+		while ((ret = read(fd, &buf, 1)))
+		{
+			i++;
+		}
+		x++;
+	}
+	return (i);
+}
 
 char	*ft_read_arg(int argc, char **argv, int i)
 {
@@ -23,7 +45,7 @@ char	*ft_read_arg(int argc, char **argv, int i)
 	char	*str;
 
 	x = 1;
-	str = malloc(sizeof(char) * 60000000000);
+	str = malloc(sizeof(char) * (ft_count(argc, argv) + 1));
 	while (x < argc)
 	{
 		fd = open(argv[x], O_RDONLY);
