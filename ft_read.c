ad2cd0c4e6485a4401b308fcb3f0da049e68403d
Maxime BOMPOIL <mbompoil@e1r2p15.42.fr>
2016-01-19 16:24:26 +0100

    read

--- ft_read.c
+++ ft_read.c
@@ -1,30 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_read_all.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbompoil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/18 17:35:57 by mbompoil          #+#    #+#             */
-/*   Updated: 2016/01/19 16:23:15 by mbompoil         ###   ########.fr       */
+/*   Created: 2016/01/19 16:06:30 by mbompoil          #+#    #+#             */
+/*   Updated: 2016/01/19 16:19:38 by mbompoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 
-char	*ft_read_arg(int argc,char **argv, int i);
-
-
-int		main(int argc, char **argv)
+char	*ft_read_arg(int argc, char **argv, int i)
 {
+	int		x;
+	int		ret;
+	int		fd;
+	char	buf;
 	char	*str;
-	int		i;
 
-	i = 0 ;
-	str = ft_read_arg(argc, argv, i);
-	printf("%s \n",str);
-	return (0);
+	x = 1;
+	str = malloc(sizeof(char) * 60000000000);
+	while (x < argc)
+	{
+		fd = open(argv[x], O_RDONLY);
+		while ((ret = read(fd, &buf, 1)))
+		{
+			str[i] = buf;
+			i++;
+		}
+		x++;
+		str[i] = '\0';
+	}
+	return (str);
 }
