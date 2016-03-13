77429f1319b2e8411b73c3e5220d98a072da7123
ullpita <ullpita42@gmail.com>
2016-03-13 15:39:31 +0000

    reduce and split fonctikons

--- ft_tetri_base_map.c
+++ ft_tetri_base_map.c
@@ -12,33 +12,16 @@
 
 #include "fillit.h"
 
-static char			*ft_tetri_base_map(char *map, int nbt, int r/*decrmenente a chaque fois quon appelle la fct et que dc il y a un '\n'*/, int a)
-{
-	char	alpha[28];
-	int		i;
-	int		n;
-	int		c;
 
+static char		*ft_tetri_base_algo(char *map, a, r, c)
+{
+	char	alpha[28]
+	int		i;
+	int		nbt;
+	
+	nbt = ft_nb_tetri
 	alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	i = 0;
-	n = 0;
-	while (n < 4)
-	{
-		if (map[i] == '\n')
-			n++;
-		i++;
-		c++;
-	}
-	while (i >= 0)
-	{
-		if (map[i] != '.' || map[i] != '\n')
-			map[i] = '.';
-	}
-	while (map[i] != alpha[a])
-	{
-		i++;
-		r++;
-	}
 	while (map[i] != '\0')
 	{
 		if (map[i] == alpha[a] && map[i - r] != '\n')
@@ -50,9 +33,34 @@ static char			*ft_tetri_base_map(char *map, int nbt, int r/*decrmenente a chaque
 			if (!(a == nbt))
 				ft_tetri_base_map(a + 1);
 			ft_adjust_map(m + 1);
-			//et ensuite refaire tetri_base_map
+			//et ensuite refaire ft_tetri_base_map()
 		}
 		i++;
+	}	
+}
+
+char			*ft_tetri_base_map(char *map, int r/*decrmenente a chaque fois quon appelle la fct et que dc il y a un '\n'*/, int a)
+{
+	char	alpha[28];
+	int		i;
+	int		n;
+	int		c;
+
+	alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	i = 0;
+	n = 0;
+	while (n < 4)
+	{
+		if (map[i] != '.' || map[i] != '\n')
+			map[i] = '.';
+		if (map[i] == '\n')
+			n++;
+		i++;
+		c++;
 	}
+	while (map[i] != alpha[a])
+		i++;
+	r = i;
+	map = ft_tetri_base_algo(*map, a, r, c);
 	return (map);
 }
