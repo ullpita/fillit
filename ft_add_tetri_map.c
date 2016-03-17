fb69d666e1f3b26cb60210a0bdb6609854792e51
Maxime BOMPOIL <mbompoil@e3r11p23.42.fr>
2016-03-17 17:36:39 +0100

    addtetru update

--- ft_add_tetri_map.c
+++ ft_add_tetri_map.c
@@ -6,47 +6,35 @@
 /*   By: upierre- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/11 14:27:42 by upierre-          #+#    #+#             */
-/*   Updated: 2016/03/17 14:06:41 by mbompoil         ###   ########.fr       */
+/*   Updated: 2016/03/17 17:36:00 by mbompoil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-#include "stdio.h"
-
 char			*ft_add_tetri_map(char **tab, char *map)
 {
 	int		i;
 	int		j;
+	int		n;
 	int		m;
 
 	i = 0;
-	m = 22000;
+	m = 21400;
 	while (tab[i])
-		i++;
-	printf("%d\n", i);
-	while (i >= 0)
 	{
-		printf("coucou2\n");
-		j = 20;
-		while (j >= 0)
+		j = 0;
+		n = 4;
+		while (n > 0)
 		{
-			printf("coucou1\n");
-			if (tab[i][j] == '\n')
-			{
-				printf("coucou3\n");
-				map[m] = '.';
-			}
-			else
-			{
-				printf("coucou4\n");
+			if (tab[i][j] != '\n')
 				map[m] = tab[i][j];
-			}
-			printf("coucou1\n");
-			j--;
-			m--;
+			if (tab[i][j] == '\n')
+				n--;
+			j++;
+			m++;
 		}
-		i--;
+		i++;
 	}
 	return (map);
 }
